b40142596052f79c1846aba4e5716871bbb1b9ad
Agustin <agustinservin1@gmail.com>
2024-10-21 15:42:38 -0300

    ejercicio 12 en desarrollo

---
+++ Ejercicio12/src/main/java/ar/edu/info/unlp/Ejercicio12/ReporteDeConstruccion.java
@@ -0,0 +1,10 @@
+package ar.edu.info.unlp.Ejercicio12;
+
+public class ReporteDeConstruccion {
+	public double volumenDeMaterial(String material) {
+		
+	}
+	public double superficieDeColor(String color) {
+		
+	}
+}
